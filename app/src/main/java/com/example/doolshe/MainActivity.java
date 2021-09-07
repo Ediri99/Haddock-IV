0f2d2fd6f572c7c273e41eab2d6c0cb69a97ab75
Ediri99 <bhanukaediri99@gmail.com>
2021-09-07 20:13:51 +0530

    welcome page without animation

---
+++ app/src/main/java/com/example/doolshe/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.doolshe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
+        getSupportActionBar().hide(); //hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //show the activity in full screen
+        setContentView(R.layout.activity_main);
+    }
+}
