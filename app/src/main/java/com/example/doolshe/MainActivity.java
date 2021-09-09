bc4f95f3ff8938ee2fcf59313e9365396120c192
Ediri99 <bhanukaediri99@gmail.com>
2021-09-09 11:33:40 +0530

    activity main (signin signup buttons.not working though)

--- app/src/main/java/com/example/doolshe/MainActivity.java
+++ app/src/main/java/com/example/doolshe/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.doolshe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView image_view;
+    TextView text2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +25,28 @@ public class MainActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); //show the activity in full screen
         setContentView(R.layout.activity_main);
+
+        image_view = (ImageView) findViewById(R.id.image_view);
+        text2 = (TextView) findViewById(R.id.text2);
+
+        image_view.animate().alpha(0f).setDuration(0);
+        text2.animate().alpha(0f).setDuration(0);
+
+        image_view.animate().alpha(1f).setDuration(1000).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                text2.animate().alpha(1f).setDuration(800);
+            }
+        });
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent  = new Intent(MainActivity.this,MainMenu.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
     }
 }
