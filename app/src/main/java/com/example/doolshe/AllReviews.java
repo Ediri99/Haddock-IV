a3cd6680994d90db8dc2a1840d50d1de1e1a2078
Ediri99 <bhanukaediri99@gmail.com>
2021-09-24 00:44:51 +0530

    Review management

--- app/src/main/java/com/example/doolshe/AllReviews.java
+++ app/src/main/java/com/example/doolshe/AllReviews.java
@@ -1,17 +1,21 @@
 package com.example.doolshe;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
-public class ShopViewCustomer extends AppCompatActivity {
+public class AllReviews extends AppCompatActivity {
 
-    Button reviews;
+    Button btnReviews;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +23,17 @@ public class ShopViewCustomer extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
         getSupportActionBar().hide(); //hide the title bar
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); //show the activity in full screen
-        setContentView(R.layout.activity_shop_view_customer);
-        reviews = (Button) findViewById(R.id.btnReviews);
-        reviews.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_all_reviews);
+
+        btnReviews = (Button) findViewById(R.id.btnAddReview);
+        btnReviews.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent addReview = new Intent(ShopViewCustomer.this,AllReviews.class);
-                startActivity(addReview);
+                Intent allReviews = new Intent(AllReviews.this,AddReview.class);
+                startActivity(allReviews);
                 /*finish();*/
             }
         });
+
     }
 }
